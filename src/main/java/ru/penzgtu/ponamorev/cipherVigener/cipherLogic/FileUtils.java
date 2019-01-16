bae9ab2be9874dce73e09557393ce7d154099da7
Vyacheslav Ponamorev <vy.ponamorev@andersenlab.com>
2019-01-16 15:51:19 +0300

    Changed draft of class which works with files

--- src/main/java/ru/penzgtu/ponamorev/cipherVigener/cipherLogic/FileUtils.java
+++ src/main/java/ru/penzgtu/ponamorev/cipherVigener/cipherLogic/FileUtils.java
@@ -1,13 +1,15 @@
 package ru.penzgtu.ponamorev.cipherVigener.cipherLogic;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileUtils {
-    public static String parseFile(File file) {
+    /*public static String parseFile(File file) {
         if (file == null) {
             System.err.println("File is NULL, specify existing file!");
             return null;
@@ -31,6 +33,26 @@ public class FileUtils {
         } catch (IOException ioEx) {
             System.err.println("There war an error during reading from file!");
             return null;
+        }
+    }
+*/
+    public static String parseFile(File file) {
+        if (file != null && file.exists()) {
+            if (!file.isDirectory()) {
+
+            }
+        }
+    }
+
+    public static void writeTableIntoFile(String cipher, File file) {
+        if (file == null) {
+            System.err.println("File is NUll, specify existing file!");
+            return;
+        }
+        try (FileWriter fileWriter = new FileWriter(file);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+        } catch (IOException ioEx) {
+            System.err.println("There was an error during writing into file!");
         }
     }
 }
